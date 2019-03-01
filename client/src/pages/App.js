a45965c271299141d917af67c63ea0c0a79939fb
Mitch Stonehocker <mtichstonehocker@gmail.com>
2019-03-01 16:16:53 -0500

    mapbox config'ed user location and draft pins

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -4,13 +4,13 @@ import React from 'react'
 import withRoot from '../withRoot'
 
 import Header from '../components/Header'
-// import Map from '../components/Map'
+import Map from '../components/Map'
 
 const App = () => {
   return (
     <>
       <Header />
-      {/* <Map /> */}
+      <Map />
     </>
   )
 }
